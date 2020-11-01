056fd1e3dc7716466d185f1dfe27c4951d23a443
shrvan93 <shrvan.abosh2000@gmail.com>
2020-11-01 14:47:20 +0200

    test1

--- weatherVue/vue.config.js
+++ weatherVue/vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/weatherVue/" : "/"
-};
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/weatherVue/'
+    : '/'
+}
