b644a9b012648d4ea6ff0f13e069fd136d0aaa73
shrvan93 <shrvan.abosh2000@gmail.com>
2020-11-01 15:33:23 +0200

    11

--- weather-vue/deploy.sh
+++ weather-vue/deploy.sh
@@ -20,6 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:shrvan93/weather-vue-app.git master:gh-pages
+ git push -f git@github.com:shrvan93/weather-vue.git master:gh-pages
 
 cd -
+
