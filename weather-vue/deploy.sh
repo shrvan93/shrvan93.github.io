807039196487a706082c4576fe77f91db094ab98
shrvan93 <shrvan.abosh2000@gmail.com>
2020-11-01 15:30:03 +0200

    wv

--- weather-vue/deploy.sh
+++ weather-vue/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:shrvan93.github.io/src/app.vue.git master:gh-pages
+ git push -f git@github.com:shrvan93/weather-vue-app.git master:gh-pages
 
 cd -
--- weather-vue/vue.config.js
+++ weather-vue/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/shrvan93.github.io/'
+    ? '/weather-vue-app/'
     : '/'
 }
